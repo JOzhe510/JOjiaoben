2c99598aa967b617e405def5637e15a35291294f
JOzhe510 <beiliyadarennb@qq.com>
2025-08-29 06:30:01 +0800

    Add files via upload

---
+++ jo脚本新.lua
@@ -0,0 +1,71 @@
+local DrRayLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/AZYsGithub/DrRay-UI-Library/main/DrRay.lua"))()
+local window = DrRayLibrary:Load("JO脚本", "Default")
+
+local tab = DrRayLibrary.newTab("通用", "ImageIdHere")
+
+tab.newSlider("修改速度", "正常为16", 9999999, false, function(Value)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
+end)
+
+tab.newSlider("跳跃高度", "正常为50", 9999999, false, function(Value)
+    game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value
+end)
+
+tab.newSlider("修改重力", "正常为90", 9999999, false, function(Value)
+    game.Workspace.Gravity = Value
+end)
+
+tab.newSlider("相机焦距", "正常为70", 250, false, function(v)
+    game.Workspace.CurrentCamera.FieldOfView = v
+end)
+
+tab.newButton("工具包", "不可见", function()
+    loadstring(game:HttpGet("https://cdn.wearedevs.net/scripts/BTools.txt"))()
+end)
+
+tab.newButton("指令脚本", "请输入文本", function()
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))()
+end)
+
+tab.newButton("NA管理员", "请输入文本", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/FilteringEnabled/NamelessAdmin/main/Source"))()
+end)
+
+tab.newButton("灵魂出窍", "注释提醒", function()
+    loadstring(game:HttpGet("https://pastebin.com/raw/ahK5jRxM"))()
+end)
+
+tab.newButton("动作变卡", "注释提醒", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/GhostPlayer352/Test4/main/Fe%20Fake%20Lag%20Obfuscator'))()
+end)
+
+tab.newButton("反复横跳", "注释提醒", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/0Ben1/fe/main/obf_11l7Y131YqJjZ31QmV5L8pI23V02b3191sEg26E75472Wl78Vi8870jRv5txZyL1.lua.txt"))()
+end)
+
+tab.newButton("定住自己", "注释提醒", function()
+    loadstring(game:HttpGet("https://pastebin.com/raw/YrfBSuWw"))()
+end)
+
+tab.newButton("无头跟断腿", "自己可见", function()
+    loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Permanent-Headless-And-korblox-Script-4140"))()
+end)
+
+tab.newButton("键盘脚本", "注释提醒", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Xxtan31/Ata/main/deltakeyboardcrack.txt", true))()
+end)
+
+tab.newButton("人物旋转", "注释提醒", function()
+    BY = "退休"
+script = "免费开源"
+QUN = "809771141"
+fling = loadstring(game:HttpGet("https://raw.githubusercontent.com/JsYb666/TUIXUI_qun-809771141/refs/heads/TUIXUI/fling"))()
+end)
+
+tab.newButton("隐身", "注释提醒", function()
+    loadstring(game:HttpGet('https://pastebin.com/raw/3Rnd9rHf'))()
+end)
+
+tab.newButton("隐身2", "注释提醒", function()
+    loadstring(game:HttpGet("https://gist.githubusercontent.com/skid123skidlol/cd0d2dce51b3f20ad1aac941da06a1a1/raw/f58b98cce7d51e53ade94e7bb460e4f24fb7e0ff/%257BFE%257D%2520Invisible%2520Tool%2520(can%2520hold%2520tools)",true))()
+end)

473a6a44de985f11cfb359f3e381e2a57abd0d7b
JOzhe510 <beiliyadarennb@qq.com>
2025-10-18 23:16:24 +0800

    Add files via upload

--- 犯罪飞行.lua
+++ 犯罪飞行.lua
@@ -13,7 +13,7 @@ local Config = {
     FixedCFrame = CFrame.new(-4895, 55, -68, 0, -1, -1, -0, 1, -1, 1, 0, -0),
     Flight = {
         SwimFly = false,
-        SwimFlySpeed = 100,
+        SwimFlySpeed = 10,
         SwimFlyVertPower = 35,
         OriginalGravity = Workspace.Gravity
     }

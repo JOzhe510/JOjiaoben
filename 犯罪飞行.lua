d3326c02d3b07cf4af39eea3472d61ce0f5fdb62
JOzhe510 <beiliyadarennb@qq.com>
2025-10-19 02:27:36 +0800

    Add files via upload

--- 犯罪飞行.lua
+++ 犯罪飞行.lua
@@ -7,14 +7,14 @@ local LocalPlayer = Players.LocalPlayer
 
 local Config = {
     Enabled = false,
-    Interval = 2,
+    Interval = 1.8,
     UsePlayerPosition = true,
     FixedVector = Vector3.new(700, 600, -2800),
     FixedCFrame = CFrame.new(-48950, 550, -680, 110, -100, -100, -10, 11, -100, 100, 111, -100),
     Flight = {
         SwimFly = false,
         SwimFlySpeed = 50,
-        SwimFlyVertPower = 50,
+        SwimFlyVertPower = 70,
         OriginalGravity = Workspace.Gravity
     }
 }
